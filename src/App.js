4ce35fe30dfa830b15027a819630ad391a2598eb
Gjeev <g_jeevika@me.iitr.ac.in>
2023-09-02 02:03:46 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ import About from './components/About/About';
 import Gallery from './components/Gallery/Gallery';
 import Contact from './components/Contact/Contact';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useEffect } from "react";
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
 
 function App() {
+  useEffect(() => {
+    MouseFollower.registerGSAP(gsap);
+    const cursor = new MouseFollower({
+      container: ".home-container",
+      speed: 0.8
+    });
+  }, []);
+
   const router = createBrowserRouter([
     { path: '/', 
     element : <Home/>,
