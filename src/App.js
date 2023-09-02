2025759fcb655971104047c8385f7b31a1c50c98
Gjeev <g_jeevika@me.iitr.ac.in>
2023-09-02 14:01:27 +0530

    custom mouse follower add

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home/home';
-import Events from './components/Events/Events';
-import About from './components/About/About';
-import Gallery from './components/Gallery/Gallery';
-import Contact from './components/Contact/Contact';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home/home";
+import Events from "./components/Events/Events";
+import About from "./components/About/About";
+import Gallery from "./components/Gallery/Gallery";
+import Contact from "./components/Contact/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useEffect } from "react";
 import MouseFollower from "mouse-follower";
 import gsap from "gsap";
+import "./components/assets/mouseFollower.css";
 
 function App() {
   useEffect(() => {
     MouseFollower.registerGSAP(gsap);
     const cursor = new MouseFollower({
-      container: ".home-container",
-      speed: 0.8
+      container: document.body,
+      speed: 0.6,
     });
   }, []);
 
   const router = createBrowserRouter([
-    { path: '/', 
-    element : <Home/>,
-    },
-    {path : '/events',
-    element : <Events />
-    },
-    {path : '/about-us',
-    element : <About />
-    },
-    {path : '/gallery',
-    element : <Gallery />
-    },
-    {path : '/contact-us',
-    element : <Contact />
-    }
-  ])
+    { path: "/", element: <Home /> },
+    { path: "/events", element: <Events /> },
+    { path: "/about-us", element: <About /> },
+    { path: "/gallery", element: <Gallery /> },
+    { path: "/contact-us", element: <Contact /> },
+  ]);
   return (
     <React.StrictMode>
       <RouterProvider router={router} />
