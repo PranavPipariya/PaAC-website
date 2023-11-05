e09030e7fb35260576d8d6435e2edeed4d9237ac
PranavPipariya <73152560+PranavPipariya@users.noreply.github.com>
2023-11-05 23:21:26 +0530

    New Member added to the Members section

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -27,6 +27,20 @@ import suryansh from "../assets/members/suryansh.png";
 import susmit from "../assets/members/susmit.png";
 import vanshaj from "../assets/members/vanshaj.png";
 import yash from "../assets/members/yash.png";
+import shivank from "..//assets/members/1682647588113 - SHIVANK GARG 22115141.png";
+import ksudharshan from "..//assets/members/20230406_161151 - K SUDHARSHAN.png";
+import hardikbhand from "..//assets/members/IMG_20220830_043758_730_1_1_2 - HARDIK BHANDARI.png";
+import GOSAVI from "..//assets/members/IMG_20230903_012938_149 - GOSAVI SUMEDH DEVESH 22123016.png";
+import VAIBHAV from "..//assets/members/IMG_20231011_000618 - VAIBHAV 22117146.png";
+import BHEEMARASETTY from "..//assets/members/IMG_20231011_160659 - BHEEMARASETTY CHAKRAVARTHY.png";
+import YUG from "..//assets/members/IMG_4327 - YUG MAHESHWARI 22113173.png";
+import khushii from "..//assets/members/Jee_photo - Khushi A..png";
+import gjeevika from "..//assets/members/Jeevika_s photo - G JEEVIKA.png";
+import SHAUN from "..//assets/members/PXL_20231004_164919368.PORTRAIT - SHAUN BUDARPUR 23615027.png";
+import kothadia from "..//assets/members/WhatsApp Image 2023-10-05 at 15.53.23_5f1352a4 - KOTHADIYA JEELBEN BALKRUSHNA 21324013.png";
+import AASTHA from "..//assets/members//WhatsApp Image 2023-10-10 at 03.05.35 - AASTHA KAPOOR.png";
+import TSEWANG from "..//assets/members/my pic pAac - TSEWANG KHASPHEL 22324024.png";
+import pranav from "..//assets/members/pranav.png";
 
 const Secys = [
     {
@@ -104,6 +118,7 @@ const Heads = [
         email : "",
         year : 4,
     },
+    
 ]
 
 const Members = [
@@ -267,6 +282,141 @@ const Members = [
         email : "",
         year : 3,
     },
+
+    {
+        name : " K Sudharshan",
+        image : ksudharshan,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    
+    {
+        name : " Shivank",
+        image : shivank,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    
+
+    {
+        name : " Hardik",
+        image : hardikbhand,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    {
+        name : " Gosavi",
+        image : GOSAVI,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    
+    {
+        name : " Pranav",
+        image : pranav,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 2,
+    },
+
+
+    {
+        name : " Vibhav",
+        image : VAIBHAV,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 2,
+    },
+
+    {
+        name : " Bheemarasetty",
+        image : BHEEMARASETTY,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    {
+        name : " Yug",
+        image : YUG,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 2,
+    },
+
+    {
+        name : " Khushi",
+        image : khushii,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 2,
+    },
+
+    {
+        name : " Jeevika",
+        image : gjeevika,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    {
+        name : " Shaun ",
+        image : SHAUN,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+
+    {
+        name : " Kothadia",
+        image : kothadia,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    {
+        name : " Aastha",
+        image : AASTHA,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 3,
+    },
+
+    {
+        name : " Tsewang",
+        image : TSEWANG,
+        position : "Member",
+        linkedIn : "",
+        email : "",
+        year : 2,
+    },
+
+
+
     {
         name : "Vanshaj Kerni",
         image : vanshaj,
@@ -275,6 +425,7 @@ const Members = [
         email : "",
         year : "4",
     },
+ 
 ]
 
 export{
