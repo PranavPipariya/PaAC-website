149c1aa0ecad775d397b8383d0ec63eedf3576d6
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-15 19:15:27 +0530

    designed newsletter on home section

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -7,6 +7,7 @@ import { NavLink } from "react-router-dom";
 import Achievement from "../Achievement/Achievement";
 import HomeEvents from "./HomeEvents";
 import Logo from "./Logo";
+import Newsletter from "./Newsletter";
 
 export default function Home() {
 
@@ -39,6 +40,9 @@ export default function Home() {
           ></img>
         </div>
         <div className="home-body">
+          <div className="newsletter-container">
+            <Newsletter/>
+          </div>
           <div className="know-paac-container">
             <div
               className="paac-desc"
