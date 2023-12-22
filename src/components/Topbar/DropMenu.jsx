7f08efc80cada548d483fc25fb97309b1bd4bfc9
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-22 09:33:45 +0530

    completed mobile menu

--- src/components/Topbar/DropMenu.jsx
+++ src/components/Topbar/DropMenu.jsx
@@ -14,11 +14,29 @@ const DropMenu = () => {
       <Popover.Portal>
         <Popover.Content className="PopoverContent" sideOffset={5}>
           <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-          <NavLink to="/">
+          <NavLink to="/" className="dropDownMenuLink">
                 <p className="menuItems">Home</p>
             </NavLink>
-            <NavLink to="/people">
+            <NavLink to="/people" className="dropDownMenuLink">
                 <p className="menuItems">About</p>
+            </NavLink>
+            <NavLink to="/library" className="dropDownMenuLink">
+                <p className="menuItems">Library</p>
+            </NavLink>
+            <NavLink to="/events" className="dropDownMenuLink">
+                <p className="menuItems">Events</p>
+            </NavLink>
+            <NavLink to="/open-projects" className="dropDownMenuLink">
+                <p className="menuItems">Open Projects</p>
+            </NavLink>
+            <NavLink to="/jc" className="dropDownMenuLink">
+                <p className="menuItems">Journal Club</p>
+            </NavLink>
+            <NavLink to="/gallery" className="dropDownMenuLink">
+                <p className="menuItems">Gallery</p>
+            </NavLink>
+            <NavLink to="/contact-us" className="dropDownMenuLink">
+                <p className="menuItems">Contact</p>
             </NavLink>
           </div>
           <Popover.Close className="PopoverClose" aria-label="Close">
